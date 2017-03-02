555108fbf9880b6087abc8b07d9623cdbd803b90
Michal Muskala <michal@muskala.eu>
2017-03-02 12:34:26 +0100

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule PersistentEts.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :persistent_ets,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package(),
+     name: "PersistentEts",
+     docs: docs()]
   end
 
   def application do
@@ -20,6 +26,25 @@ defmodule PersistentEts.Mixfile do
   defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev}]
+  end
+
+  defp description do
+    """
+    Ets table backed by a persistence file
+    """
+  end
+
+  defp package do
+    [maintainers: ["Michał Muskała"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/michalmuskala/persistent_ets"}]
+  end
+
+  defp docs do
+    [source_ref: "v#{@version}",
+     canonical: "http://hexdocs.pm/persistent_ets",
+     source_url: "https://github.com/michalmuskala/persistent_ets",
+     main: "PersistentEts"]
   end
 end
