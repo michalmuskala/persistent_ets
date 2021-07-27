0f2f32fd8be8b08b9439a5cb368f9418bf6f7e8b
Kian-Meng, Ang <kianmeng@cpan.org>
2021-07-27 20:27:42 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,19 +1,24 @@
 # Changelog
 
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
 ## 0.2.1 (14.08.2019)
 
 ### Bug fixes
 
-* fix `PersistentEts.new/3` (broken since `0.2.0`).
+* Fix `PersistentEts.new/3` (broken since `0.2.0`).
 
 ## 0.2.0 (02.08.2019)
 
 ### Changes
 
-* use `DynamicSupervisor` instead of `:simple_one_for_one` and migrate to the
+* Use `DynamicSupervisor` instead of `:simple_one_for_one` and migrate to the
   Elixir 1.5 child specs.
 * Improve documentation.
 
 ## 0.1.0 (02.03.2017)
 
-Initial release
+* Initial release
--- lib/persistent_ets.ex
+++ lib/persistent_ets.ex
@@ -1,31 +1,8 @@
 defmodule PersistentEts do
-  @moduledoc """
-  Ets table backed by a persistence file
-
-  The table is persisted using the `:ets.file2tab/2` and `:ets.tab2file/3`
-  functions.
-
-  Table is to be created with `PersistentEts.new/3` in place of `:ets.new/2`.
-  After that all functions from `:ets` can be used like with any other table,
-  except `:ets.give_away/3` and `:ets.delete/1` - replacement functions are
-  provided in this module. The `:ets.setopts/2` function to change the heir
-  is not supported - the heir setting is leveraged by the persistence mechanism.
-
-  Like with regular ets table, the table is destroyed once the owning process
-  (the one that called `PersistentEts.new/3`) dies, but the table data is persisted
-  so it will be re-read when table is opened again.
-
-  ## Example
-
-      pid = spawn(fn ->
-        :foo = PersistentEts.new(:foo, "table.tab", [:named_table])
-        :ets.insert(:foo, [a: 1])
-      end)
-      Process.exit(pid, :diediedie)
-      PersistentEts.new(:foo, "table.tab", [:named_table])
-      [a: 1] = :ets.tab2list(:foo)
-
-  """
+  @external_resource "README.md"
+  @moduledoc File.read!("README.md")
+             |> String.split(~r/<!-- MDOC !-->/)
+             |> Enum.fetch!(1)
 
   @type tab :: :ets.tab
   @type type :: :ets.type
